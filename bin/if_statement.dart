7e89498b0920275543fb98e0fb3d7a1e779bcacb
awalsetya <awalsetyanto.1993@gmail.com>
2020-06-30 23:57:52 +0700

    if & else condition

---
+++ bin/if_statement.dart
@@ -0,0 +1,12 @@
+void main() {
+  var isRaining = true;
+
+  print('Prepare before going iffice');
+  if (isRaining) {
+    print('Bawa payung');
+  }
+  print('going to the office');
+}
+
+// ekpresi if akan menguji suatu kondisi. Jika hasil ekspresi bernilai true, maka blok kode di dalamnya
+// akan di jalankan. jika bernilai false maka proses yang di tentukan akan di lewatkan;
